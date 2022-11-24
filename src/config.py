ab160c6e989e1a349fe8173cc11f413b488c747f
XJTUXYC <1479018183@qq.com>
2022-11-24 19:39:36 +0800

    M

--- src/config.py
+++ src/config.py
@@ -263,7 +263,7 @@ parser.add_argument('--use_GRU',
                     help="use GRU")
 parser.add_argument('--always_clamp',
                     action='store_true',
-                    default=True,
+                    default=False,
                     help="always clamp")
 parser.add_argument('--zero_init_GRU',
                     action='store_true',
