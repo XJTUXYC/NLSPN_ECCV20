a5b7c348dc3805d6dc24683bbb61d45c467a32ce
XJTUXYC <1479018183@qq.com>
2022-12-26 19:46:18 +0800

    M

--- src/model/nlspnmodel.py
+++ src/model/nlspnmodel.py
@@ -309,9 +309,9 @@ class NLSPNModel(nn.Module):
         pred = self.dec11_pred(concat(fd1_pred_conf, fe1)) # b*(64+64)*H*W -> b*1*H*W
         
         if self.args.preserve_input:
-            mask_fix = torch.sum(dep > self.args.min_depth, dim=1, keepdim=True).detach()
+            mask_fix = torch.sum((dep >= self.args.min_depth) & (dep <= self.args.max_depth), dim=1, keepdim=True).detach()
             mask_fix = (mask_fix > 0.0).type_as(dep)
-            pred = (1.0 - mask_fix) * pred + mask_fix * (self.args.min_depth / (dep + 1e-4) - self.args.min_depth / self.args.max_depth)
+            pred = (1.0 - mask_fix) * pred + mask_fix * (self.args.min_depth / (dep + 1e-8) - self.args.min_depth / self.args.max_depth)
         if self.args.always_clip:
             pred = torch.clamp(pred, min=0, max=1-self.args.min_depth/self.args.max_depth)
             
@@ -349,8 +349,11 @@ class NLSPNModel(nn.Module):
         # print('time cost for GRU1',1000*(time_end-time_start),'ms')
         
         # output
+        if not self.args.always_clip:
+            pred = torch.clamp(pred, min=0, max=1-self.args.min_depth/self.args.max_depth)
+        
         pred = self.args.min_depth / (pred + self.args.min_depth / self.args.max_depth)
-        pred = torch.clamp(pred, min=self.args.min_depth, max=self.args.max_depth)
+        # pred = torch.clamp(pred, min=self.args.min_depth, max=self.args.max_depth)
         output = {'pred': pred}
 
         return output
